277594186007f6fc92d0d18fa573f164919f1747
patrickryan <theyoungsoul@gmail.com>
2019-06-30 16:19:15 -0500

    updated pi_objectdetect_and_stream with async sender

--- pi_objectdetect_and_stream.py
+++ pi_objectdetect_and_stream.py
@@ -10,6 +10,7 @@ from imagezmq.asyncimagesender import AsyncImageSender
 # get the host name, initialize the video stream, and allow the
 # camera sensor to warmup
 rpiName = socket.gethostname()
+print(f"Running as host: {rpiName}")
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -21,6 +22,7 @@ args = vars(ap.parse_args())
 # initialize the ImageSender object with the socket address of the
 # server
 if args['server_ip']:
+    print("Initialize AsyncImageSender...")
     sender = AsyncImageSender(server_name=rpiName, server_ip=args['server_ip'], port=5555, send_timeout=10, recv_timeout=10)
     sender.run_in_background()
 
@@ -41,4 +43,5 @@ detector = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
 object_detection = ObjectDetection(use_pi_camera=True, recognize_faces=True, face_detector=detector,
                                    frame_callback=cb, detection_method='hog', encodings_files='./encodings/friends_family_encodings.pkl')
 
+print("Starting image detection")
 object_detection.detect_objects()
