9a8282c62f4304a21dea6b7b755402f8d997292f
patrickryan <theyoungsoul@gmail.com>
2019-06-30 16:08:42 -0500

    updated pi_objectdetect_and_stream with async sender

--- pi_objectdetect_and_stream.py
+++ pi_objectdetect_and_stream.py
@@ -1,18 +1,18 @@
 from ObjectDetection import ObjectDetection
 import cv2
-from imagezmq.imagezmq import ImageSender
+from imagezmq.asyncimagesender import AsyncImageSender
 import socket
 import time
-import signal
-from contextlib import contextmanager
 import argparse
-import threading
 from queue import Queue
 
 frame_queue = Queue()
 
-
 timestamp_of_last_socket_refresh = time.time()
+
+# get the host name, initialize the video stream, and allow the
+# camera sensor to warmup
+rpiName = socket.gethostname()
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -24,40 +24,13 @@ args = vars(ap.parse_args())
 # initialize the ImageSender object with the socket address of the
 # server
 if args['server_ip']:
-    sender = ImageSender(connect_to="tcp://{}:5555".format(
-        args["server_ip"]), send_timeout=10, recv_timeout=10)
+    sender = AsyncImageSender(server_name=rpiName, server_ip=args['server_id'], port=5555, send_timeout=10, recv_timeout=10)
 else:
     sender = None
 
-# get the host name, initialize the video stream, and allow the
-# camera sensor to warmup
-rpiName = socket.gethostname()
-
-
-def send_frame_background_function(sender):
-    while True:
-        frame = frame_queue.get()
-        try:
-            try:
-                hub_reply = sender.send_image(rpiName, frame)
-            except Exception as exc:
-                print("send_image exception")
-                print(f"Exception msg: {exc}")
-                time.sleep(6)  # something happened, force a timeout
-                raise TimeoutError
-        except TimeoutError:
-            print("Sending timeout.. reconnect to server")
-            sender = ImageSender(connect_to="tcp://{}:5555".format(
-                args["server_ip"]), send_timeout=10, recv_timeout=10)
-
-
-x = threading.Thread(target=send_frame_background_function, args=(sender,))
-x.daemon = True
-x.start()
-
 
 def new_frame_callback(frame):
-    frame_queue.put_nowait(frame)
+    sender.send_frame_async(frame)
 
 
 cb = new_frame_callback
